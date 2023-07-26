6f07ec674b79a8c67b215e2f0ccccda8f65e1fcd
Gabriel Oest <gabriel.oest@etermax.com.uy>
2023-07-26 00:29:19 -0300

    Add Body Parser use configuration

--- index.ts
+++ index.ts
@@ -1,11 +1,15 @@
 import express from 'express';
+import bodyParser from 'body-parser';
 
 import { AdminRoute, VendorRoute } from './routes';
 
 const app = express();
 
-app.use("/admin", AdminRoute)
-app.use("/vendor", VendorRoute)
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.use("/admin", AdminRoute);
+app.use("/vendor", VendorRoute);
 
 app.use("/", (req, res) => {
     return res.json("Hello from Food Order Backend!");
