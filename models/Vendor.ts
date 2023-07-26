2ae0a4a09c2ee99a74d617dacde88831fdccaac7
Gabriel Oest <gabriel.oest@etermax.com.uy>
2023-07-26 00:45:12 -0300

    Add mongoDB Vendor schema and document

---
+++ models/Vendor.ts
@@ -0,0 +1,37 @@
+import mongoose, {Schema, Document, Model} from "mongoose";
+
+interface VendorDoc extends Document {
+    name: string;
+    ownerName: string;
+    foodType: [string];
+    pincode: string;
+    address: string;
+    phone: string;
+    email: string;
+    password: string;
+    salt: string;
+    serviceAvailable: boolean;
+    coverImage: [string];
+    rating: number;
+}
+
+const VendorSchema = new Schema({
+    name: {type: String, required: true},
+    ownerName: {type: String, required: true},
+    foodType: {type: [String]},
+    pincode: {type: String, required: true},
+    address: {type: String},
+    phone: {type: String, required: true},
+    email: {type: String, required: true},
+    password: {type: String, required: true},
+    salt: {type: String, required: true},
+    serviceAvailable: {type: Boolean},
+    coverImage: {type: [String]},
+    rating: {type: Number},
+},{
+    timestamps: true
+})
+
+const Vendor = mongoose.model<VendorDoc>("vendor", VendorSchema);
+
+export {Vendor}
