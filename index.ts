638c6979da1ef4f7c457b9204001332192ced72c
Gabriel Oest <gabriel.oest@etermax.com.uy>
2023-07-26 00:54:52 -0300

    Add mongoDB connection configuration

--- index.ts
+++ index.ts
@@ -1,7 +1,9 @@
 import express from 'express';
 import bodyParser from 'body-parser';
+import mongoose, { ConnectOptions } from 'mongoose';
 
 import { AdminRoute, VendorRoute } from './routes';
+import { MONGO_URL } from './config';
 
 const app = express();
 
@@ -14,6 +16,11 @@ app.use("/vendor", VendorRoute);
 app.use("/", (req, res) => {
     return res.json("Hello from Food Order Backend!");
 });
+
+mongoose.connect(MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+} as ConnectOptions);
 
 app.listen(8000, () => {
     console.log("App is running on port 8000");
