35ed6bd9b7b49ae731e330102e7f5581402c5c39
Gabriel Oest <gabriel.oest@etermax.com.uy>
2023-07-26 01:04:28 -0300

    Add validation for existing vendor for email on CreateVendor

--- controllers/AdminController.ts
+++ controllers/AdminController.ts
@@ -5,6 +5,12 @@ import { Vendor } from '../models';
 export const CreateVendor =async (req:Request, res: Response, next: NextFunction) => {
     const {name, address, pincode, foodType, email, password, ownerName, phone} = <CreateVendorInputs> req.body;
 
+    const existingVendor = Vendor.findOne({email: email});
+
+    if(existingVendor !== null){
+        return res.json({message: "A vendor with the provided email already exists"});
+    }
+
     const  createdVendor = await Vendor.create({
         name: name,
         address: address,
