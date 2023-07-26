ecfed5439c524aefa3d1ab1056b4ef7d6ce276aa
Gabriel Oest <gabriel.oest@etermax.com.uy>
2023-07-26 00:59:46 -0300

    Add save Vendor on DB on CreateVendor

--- controllers/AdminController.ts
+++ controllers/AdminController.ts
@@ -1,8 +1,24 @@
 import express, {Request, Response, NextFunction} from 'express'
 import { CreateVendorInputs } from '../dto'
+import { Vendor } from '../models';
 
 export const CreateVendor =async (req:Request, res: Response, next: NextFunction) => {
     const {name, address, pincode, foodType, email, password, ownerName, phone} = <CreateVendorInputs> req.body;
 
-    res.json({name: name})
+    const  createdVendor = await Vendor.create({
+        name: name,
+        address: address,
+        pincode: pincode,
+        foodType: foodType,
+        email: email,
+        password: password,
+        salt: "salt",
+        ownerName: ownerName,
+        phone: phone,
+        rating: 0,
+        serviceAvailable: false,
+        coverImage: []
+    })
+
+    res.json(createdVendor)
 }
